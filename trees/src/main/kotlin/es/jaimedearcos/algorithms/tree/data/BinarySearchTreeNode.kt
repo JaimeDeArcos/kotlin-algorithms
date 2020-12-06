10cb504b7f28d30c3d4c966384a9e1741edcbb40
jdearcossanchez <jdearcossanchez@twilio.com>
2020-12-06 11:35:59 +0100

    Binary Search Tree

---
+++ trees/src/main/kotlin/es/jaimedearcos/algorithms/tree/data/BinarySearchTreeNode.kt
@@ -0,0 +1,62 @@
+package es.jaimedearcos.algorithms.tree.data
+
+data class BinarySearchTreeNode(
+        var data:Int,
+        var left: BinarySearchTreeNode? = null,
+        var right: BinarySearchTreeNode? = null
+){
+
+    fun insert(value:Int){
+        if(value <= data && left != null){
+            left!!.insert(value)
+        }else if (value <= data){
+            left = BinarySearchTreeNode(value)
+        }else if(value> data && right != null){
+            right!!.insert(value)
+        }else{
+            right = BinarySearchTreeNode(value)
+        }
+    }
+
+    fun contains(value:Int) : Boolean{
+        if (value < data && left !=null) {
+            return left!!.contains(value)
+        }
+        if(value > data && right!=null) {
+            return right!!.contains(value)
+        }
+        return value==data
+    }
+
+    fun remove(value:Int, parent:BinarySearchTreeNode) : Boolean{
+        if (value<data && left!=null){
+            return left!!.remove(value,this)
+        }else if(value<data){
+            return false
+        }else if(value>data && right != null){
+            return left!!.remove(value,this)
+        }else if (value>data){
+            return false
+        }else {
+            if (left == null && right==null && this == parent.left) {
+                parent.left = null
+            }else if (left == null && right==null && this==parent.right){
+                parent.right = null
+            }else if(left!=null && right==null && this == parent.left){
+                parent.left = this.left
+            }else if(left!=null && right== null && this == parent.right){
+                parent.right = this.left
+            }else if (right!=null && left==null && this == parent.left) {
+                parent.left = this.right
+            }else if(right!=null && left==null && this == parent.right) {
+                parent.right = this.right
+            }else{
+                data = right!!.min()
+                right!!.remove(data,this)
+            }
+            return true
+        }
+    }
+
+    fun min() : Int = if (left==null) data else left!!.min()
+}
